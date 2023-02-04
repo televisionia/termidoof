f405b4690962bac342cc94c16ca3a857a5a13bf4
Televisionia <106242960+televisionia@users.noreply.github.com>
2023-02-04 16:54:38 +1100

    Changed how messages from client are sent

--- servermanager.py
+++ servermanager.py
@@ -81,32 +81,33 @@ def ServerLoop(server, SocketConnection, Address):
         
         # - - - - - - - - -
         #This is where commands that go to the server are handled
-        match SplitInput[0]:
-            case "CM":
-                match SplitInput[4]:
-                    case "msg":
-                        SocketConnection.send(f"Read input as: {SplitInput}".encode('utf-8'))
-                        FoundUser = GetUserFromID[SplitInput[1], GlobalUserList]
-                        SplitInput.pop(0)
-                        SplitInput.pop(1)
-                        for ConnectedClient in GlobalUserList:
-                            SocketConnection.sendto(f"[{FoundUser.colorcode}{FoundUser.username}\033[0m]: {SplitInput[4]}".encode('utf-8'), ConnectedClient[0].address)
-                    case "userlist":
-                        SocketConnection.send(f"Read input as: {SplitInput}".encode('utf-8'))
-                        for ConnectedClient in GlobalUserList:
-                            SocketConnection.send(f"ID{ConnectedClient[1]}: {ConnectedClient[0].colorcode}{ConnectedClient[0].username}\033[0m")
-            case "UD":
-                NewID = GlobalUserIDCount
-                GlobalUserIDCount += 1
+        
+        if SplitInput[0] == "UD": #USERDATA TRANSFER
+            
+            NewID = GlobalUserIDCount
+            GlobalUserIDCount += 1
+            NewUserAddress = (SplitInput[3], int(SplitInput[4]))
+            
+            GlobalUserList.append([User(SplitInput[1], SplitInput[2], NewUserAddress), NewID])
+            SocketConnection.send(str(NewID).encode('utf-8'))
+            
+            for ConnectedClient in GlobalUserList:
+                SocketConnection.sendto(f"{SplitInput[1]} has entered the server.".encode('utf-8'), ConnectedClient[0].address)
                 
-                NewUserAddress = (SplitInput[3], int(SplitInput[4]))
-                
-                GlobalUserList.append([User(SplitInput[1], SplitInput[2], NewUserAddress), NewID])
-                
-                SocketConnection.send(str(NewID).encode('utf-8'))
-                
-                for ConnectedClient in GlobalUserList:
-                    SocketConnection.sendto(f"{SplitInput[1]} has entered the server.".encode('utf-8'), ConnectedClient[0].address)
+        elif SplitInput[0] == "CM": #CLIENT MESSAGES OR COMMANDS
+            
+            match SplitInput[4]:
+                case "msg":
+                    FoundUser = GetUserFromID[SplitInput[1], GlobalUserList]
+                    SplitInput.pop(0)
+                    SplitInput.pop(0)
+                    SplitInput.pop(0)
+                    SplitInput.pop(0)
+                    for ConnectedClient in GlobalUserList:
+                        SocketConnection.sendto(f"[{FoundUser.colorcode}{FoundUser.username}\033[0m]: {' '.join(SplitInput)}".encode('utf-8'), ConnectedClient[0].address)
+                case "userlist":
+                    for ConnectedClient in GlobalUserList:
+                        SocketConnection.send(f"ID{ConnectedClient[1]}: {ConnectedClient[0].colorcode}{ConnectedClient[0].username}\033[0m")
                 
         # - - - - - - - - -
 
